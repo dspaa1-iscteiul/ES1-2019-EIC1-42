bd80704ae84036772061f43dc1f71ffc5d5d76fe
dspaa1-iscteiul <dspaa1@iscte-iul.pt>
2019-11-01 23:23:13 +0000

    Inicial Maven Structure

---
+++ ES1-2019-EIC1-42/src/main/java/pt/iul/ista/ES1_2019_EIC1_42/App.java
@@ -0,0 +1,13 @@
+package pt.iul.ista.ES1_2019_EIC1_42;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

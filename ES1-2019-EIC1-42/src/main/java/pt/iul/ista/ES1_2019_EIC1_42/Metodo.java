1a13409d815f5028f8bd36c87a0b85fd26576abf
rmdca-iscteiul <rmdca@iscte-iul.pt>
2019-11-05 16:43:24 +0000

    Atributos da classe Metodo
    Criação dos atributos da classe Metodo

--- ES1-2019-EIC1-42/src/main/java/pt/iul/ista/ES1_2019_EIC1_42/Metodo.java
+++ ES1-2019-EIC1-42/src/main/java/pt/iul/ista/ES1_2019_EIC1_42/Metodo.java
@@ -1,5 +1,21 @@
 package pt.iul.ista.ES1_2019_EIC1_42;
 
 public class Metodo {
+	
+	private int methodid;
+	private String pacote;
+	private String classe;
+	private String method;
+	private int loc;
+	private int cyclo;
+	private int atfd;
+	private int laa;
+	private	boolean is_long_method;
+	private boolean iplasma;
+	private	boolean pmd;
+	private boolean is_feature_envy;
+	
+	
+	
 
 }

e85818e9c4d5341ebd51af91cc857e140f0a29ae
rmdca-iscteiul <rmdca@iscte-iul.pt>
2019-11-06 10:59:15 +0000

    Getters adicionados

--- ES1-2019-EIC1-42/src/main/java/pt/iul/ista/ES1_2019_EIC1_42/Metodo.java
+++ ES1-2019-EIC1-42/src/main/java/pt/iul/ista/ES1_2019_EIC1_42/Metodo.java
@@ -1,8 +1,8 @@
 package pt.iul.ista.ES1_2019_EIC1_42;
 
 public class Metodo {
-	
-	private int methodid;
+
+	private int methodID;
 	private String pacote;
 	private String classe;
 	private String method;
@@ -10,12 +10,53 @@ public class Metodo {
 	private int cyclo;
 	private int atfd;
 	private int laa;
-	private	boolean is_long_method;
+	private boolean is_long_method;
 	private boolean iplasma;
-	private	boolean pmd;
+	private boolean pmd;
 	private boolean is_feature_envy;
-	
-	
-	
+
+	public int getMethodID() {
+		return methodID;
+	}
+
+	public String getClasse() {
+		return classe;
+	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	public int getLoc() {
+		return loc;
+	}
+
+	public int getCyclo() {
+		return cyclo;
+	}
+
+	public int getAtfd() {
+		return atfd;
+	}
+
+	public int getLaa() {
+		return laa;
+	}
+
+	public boolean isLongMethod() {
+		return is_long_method;
+	}
+
+	public boolean isIplasma() {
+		return iplasma;
+	}
+
+	public boolean isPmd() {
+		return pmd;
+	}
+
+	public boolean isFeatureEnvy() {
+		return is_feature_envy;
+	}
 
 }

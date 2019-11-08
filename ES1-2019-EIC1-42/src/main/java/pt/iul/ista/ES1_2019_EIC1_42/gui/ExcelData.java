91ae1050a483c9e483494126750f50e689b5165d
aaspo-iscteiul <aaspo@iscte-iul.pt>
2019-11-08 13:09:11 +0000

    JFileChooser exceldata

--- ES1-2019-EIC1-42/src/main/java/pt/iul/ista/ES1_2019_EIC1_42/gui/ExcelData.java
+++ ES1-2019-EIC1-42/src/main/java/pt/iul/ista/ES1_2019_EIC1_42/gui/ExcelData.java
@@ -2,10 +2,13 @@ package pt.iul.ista.ES1_2019_EIC1_42.gui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.io.File;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  * Classe para visualizar um ficheiro Excel
@@ -13,7 +16,7 @@ import javax.swing.border.EmptyBorder;
  *
  */
 public class ExcelData extends JFrame {
-
+	private File excelfile;
 	private JPanel contentPane;
 
 	/**
@@ -31,6 +34,7 @@ public class ExcelData extends JFrame {
 			}
 		});
 	}
+	
 
 	/**
 	 * Create the frame.
@@ -42,6 +46,17 @@ public class ExcelData extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		fileChooser();
+	}
+	
+	//create JFileChooser
+	
+	public void fileChooser() {
+		JFileChooser filechooser = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(".xlsx", "xlsx", "excel");
+		filechooser.setFileFilter(filter);
+		filechooser.showOpenDialog(null);	
+		excelfile = filechooser.getSelectedFile();
 	}
 
 }

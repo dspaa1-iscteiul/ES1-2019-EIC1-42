1e88b276c9ff46e3e3be719e63f4016466d2b3b3
Eric Sancha <elsas@iscte-iul.pt>
2019-11-08 13:36:37 +0000

    comeco

--- ES1-2019-EIC1-42/src/main/java/pt/iul/ista/ES1_2019_EIC1_42/DataModel.java
+++ ES1-2019-EIC1-42/src/main/java/pt/iul/ista/ES1_2019_EIC1_42/DataModel.java
@@ -3,18 +3,31 @@ package pt.iul.ista.ES1_2019_EIC1_42;
 import java.util.ArrayList;
 
 import javax.swing.table.AbstractTableModel;
+import java.io.File;  
+import java.io.FileInputStream;  
+import java.util.Iterator;  
+import org.apache.poi.ss.usermodel.Cell;  
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;  
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;  
 
 /**
  * Classe para gerir os dados que são importados do ficheiro Excel e são passados para uma JTable
- * @author dariop
+ * @author elsas
  *
  */
 public class DataModel extends AbstractTableModel {
 	
 	private ArrayList<Metodo> metodos;
-	
-	public void uploadDataBase() {
-		
+	private static DataModel INSTANCE;
+	private String[] colunas = {"MethodID","package","class","method","LOC","CYCLO",
+			"ATFD","LAA","is_long_method","iPlasma","PMD","is_future_envy"};
+	private DataModel() {
+		INSTANCE = this;
+	}
+	public void readFile() {
+		Workbook workbook = WorkbookFactory.create(new File(SAMPLE_XLSX_FILE_PATH));
 	}
 
 	public int getColumnCount() {

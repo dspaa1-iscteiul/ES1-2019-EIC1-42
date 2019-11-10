520c47ee76532aeaf1b50cd5f3667db7a062a5f7
fmpts-iscteiul <fmpts@iscte-iul.pt>
2019-11-10 18:11:22 +0000

    AllTests/ExcelDataTest

---
+++ ES1-2019-EIC1-42/src/test/java/pt/iul/ista/ES1_2019_EIC1_42/ExcelDataTest.java
@@ -0,0 +1,30 @@
+package pt.iul.ista.ES1_2019_EIC1_42;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import pt.iul.ista.ES1_2019_EIC1_42.gui.ExcelData;
+
+
+/**
+ * @author fmpts
+ *
+ */
+
+public class ExcelDataTest {
+	
+	private ExcelData frame;
+	
+	/**
+	 * Tests the class as a whole. 
+	 * Test focused on the correct initialization of ExcelData object
+	*/
+	@Test
+	public void ConstructorTest() {
+		frame = new ExcelData();
+		frame.start();
+		assertTrue(frame.getContentPane() != null);
+		assertTrue(frame.getTable() != null);
+	}
+}

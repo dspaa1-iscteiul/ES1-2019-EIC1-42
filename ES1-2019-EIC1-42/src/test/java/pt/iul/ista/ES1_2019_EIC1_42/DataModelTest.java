a5673d146cf5022a1d49ddd75902148139f6a11b
dspaa1-iscteiul <dspaa1@iscte-iul.pt>
2019-11-10 18:41:42 +0000

    Base para o DataModelTest

---
+++ ES1-2019-EIC1-42/src/test/java/pt/iul/ista/ES1_2019_EIC1_42/DataModelTest.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package pt.iul.ista.ES1_2019_EIC1_42;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author dariop
+ *
+ */
+public class DataModelTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link pt.iul.ista.ES1_2019_EIC1_42.DataModel#getContent()}.
+	 */
+	@Test
+	public void testGetContent() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pt.iul.ista.ES1_2019_EIC1_42.DataModel#getColumnCount()}.
+	 */
+	@Test
+	public void testGetColumnCount() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pt.iul.ista.ES1_2019_EIC1_42.DataModel#getRowCount()}.
+	 */
+	@Test
+	public void testGetRowCount() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pt.iul.ista.ES1_2019_EIC1_42.DataModel#getColumnName(int)}.
+	 */
+	@Test
+	public void testGetColumnNameInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pt.iul.ista.ES1_2019_EIC1_42.DataModel#getValueAt(int, int)}.
+	 */
+	@Test
+	public void testGetValueAt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pt.iul.ista.ES1_2019_EIC1_42.DataModel#getInstance()}.
+	 */
+	@Test
+	public void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+}

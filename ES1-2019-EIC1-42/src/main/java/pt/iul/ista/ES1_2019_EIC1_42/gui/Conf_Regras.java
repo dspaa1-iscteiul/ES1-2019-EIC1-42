c614da007f79da351a18fd329240a3beb9ac7186
dspaa1-iscteiul <dspaa1@iscte-iul.pt>
2019-11-19 14:33:26 +0000

    Regras inicial

--- ES1-2019-EIC1-42/src/main/java/pt/iul/ista/ES1_2019_EIC1_42/gui/Conf_Regras.java
+++ ES1-2019-EIC1-42/src/main/java/pt/iul/ista/ES1_2019_EIC1_42/gui/Conf_Regras.java
@@ -7,26 +7,28 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
 
 /**
  * Dialogo para definir as regras e limites (thresholds)
  * @author dariop
  *
  */
-public class Regras extends JDialog {
+public class Conf_Regras extends JDialog {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -2326177220283427980L;
 	private final JPanel contentPanel = new JPanel();
+	private JPanel longMethodPanel;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		try {
-			Regras dialog = new Regras();
+			Conf_Regras dialog = new Conf_Regras();
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
@@ -37,13 +39,20 @@ public class Regras extends JDialog {
 	/**
 	 * Create the dialog.
 	 */
-	public Regras() {
+	public Conf_Regras() {
 		setModalityType(ModalityType.APPLICATION_MODAL);
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		longMethodPanel = new JPanel();
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		{
+			JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+			contentPanel.add(tabbedPane);
+			
+			tabbedPane.add(longMethodPanel, "long method");
+		}
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));

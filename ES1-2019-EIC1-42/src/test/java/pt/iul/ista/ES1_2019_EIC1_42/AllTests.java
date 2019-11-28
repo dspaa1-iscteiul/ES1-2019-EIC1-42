95a9242b5719a3b66256efa54d4e2ec9b5ac4252
dspaa1-iscteiul <dspaa1@iscte-iul.pt>
2019-11-28 19:29:59 +0000

    AllTests atualizado

--- ES1-2019-EIC1-42/src/test/java/pt/iul/ista/ES1_2019_EIC1_42/AllTests.java
+++ ES1-2019-EIC1-42/src/test/java/pt/iul/ista/ES1_2019_EIC1_42/AllTests.java
@@ -5,11 +5,17 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import pt.iul.ista.ES1_2019_EIC1_42.gui.Nova_RegraTest;
+
 @RunWith(Suite.class)
 @SuiteClasses({
+	AppTest.class,
+	Comparador_de_QualidadeTest.class,
+	DataModelTest.class,
 	ExcelDataTest.class,
 	MetodoTest.class,
-	DataModelTest.class,
+	RegraTest.class,
+	Nova_RegraTest.class,	
 })
 
 public class AllTests {

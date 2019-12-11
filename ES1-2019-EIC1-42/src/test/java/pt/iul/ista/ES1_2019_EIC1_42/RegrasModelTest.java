ff31647eb0e69f77ef000b20022e263f70b42366
rmdca-iscteiul <rmdca@iscte-iul.pt>
2019-12-11 19:52:41 +0000

    Criação da classe de testes para o RegrasModel

---
+++ ES1-2019-EIC1-42/src/test/java/pt/iul/ista/ES1_2019_EIC1_42/RegrasModelTest.java
@@ -0,0 +1,64 @@
+package pt.iul.ista.ES1_2019_EIC1_42;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class RegrasModelTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetElementAt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddRegra() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveRegra() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAtualizar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRegras() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+}

546ba3394ceebf5c7330da4b9d8239993f940b15
Eric Sancha <elsas@iscte-iul.pt>
2019-12-12 21:31:50 +0000

    ExcelData, CompareTable, App

--- ES1-2019-EIC1-42/src/test/java/pt/iul/ista/ES1_2019_EIC1_42/AllTests.java
+++ ES1-2019-EIC1-42/src/test/java/pt/iul/ista/ES1_2019_EIC1_42/AllTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import pt.iul.ista.ES1_2019_EIC1_42.gui.ExcelDataTest;
 import pt.iul.ista.ES1_2019_EIC1_42.gui.Nova_RegraTest;
 
 @RunWith(Suite.class)
---
+++ ES1-2019-EIC1-42/src/test/java/pt/iul/ista/ES1_2019_EIC1_42/gui/ExcelDataTest.java
@@ -0,0 +1,45 @@
+package pt.iul.ista.ES1_2019_EIC1_42.gui;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ExcelDataTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	//Entendi como querias q testasse isso junto com a app data pq podia fazer openFile ali e testar todas essas cenas ali, mas na mesma tinha de criar a gui
+	@Test
+	public void testExcelData() {
+		fail("Not yet implemented"); 
+	}
+
+	@Test
+	public void testOpenFile() {
+		fail("Not yet implemented"); 
+	}
+
+	@Test
+	public void testGetTable() {
+		fail("Not yet implemented");
+	}
+
+}
